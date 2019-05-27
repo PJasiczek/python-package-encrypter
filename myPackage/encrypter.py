c62154a29f3367ee7d0a664935fb0d76ce1ce723
pjasiczek225933 <pjasiczek225933@e-science.pl>
2019-05-27 19:41:13 +0200

    0.1 version package import

---
+++ myPackage/encrypter.py
@@ -0,0 +1,76 @@
+import math
+import sys
+import numpy
+
+
+class Encrypter:
+
+    def encryptText(self, encryptSquare, textToEncrypt):
+
+        row = math.ceil(math.sqrt(len(encryptSquare)))
+
+        j = 0
+        w = 0
+        i = 0
+
+        pRowIndex = 0
+        pColumnIndex = 0
+        qRowIndex = 0
+        qColumnIndex = 0
+
+        squareEncrypter = [[0 for x in range(row)] for y in range(row)]
+
+        result = ""
+
+        for x in range(len(encryptSquare)):
+            if ((i == (row * j))):
+                j = j + 1
+                w = 0
+            else:
+                squareEncrypter[j - 1][w] = encryptSquare[i]
+                w = w + 1
+                i = i + 1
+
+        lenghtResultText = 1
+
+        while lenghtResultText < len(textToEncrypt):
+
+            p = textToEncrypt[lenghtResultText - 1]
+            q = textToEncrypt[lenghtResultText]
+
+            for z in range(row):
+                for x in range(row):
+                    if(squareEncrypter[z][x] == p):
+                        pRowIndex = z
+                        pColumnIndex = x
+                    if(squareEncrypter[z][x] == q):
+                        qRowIndex = z
+                        qColumnIndex = x
+
+
+            if ((pColumnIndex == qColumnIndex) and (p != q)):
+                if ((qRowIndex + 1) >= row or (pRowIndex + 1) >= row):
+                    result += squareEncrypter[0][pColumnIndex]
+                    result += squareEncrypter[0][qColumnIndex]
+                else:
+                    result += squareEncrypter[pRowIndex + 1][pColumnIndex]
+                    result += squareEncrypter[qRowIndex + 1][qColumnIndex]
+
+            elif ((pRowIndex == qRowIndex) and (p != q)):
+                if ((qColumnIndex + 1) >= row or (pColumnIndex + 1) >= row):
+                    result += squareEncrypter[pRowIndex][0]
+                    result += squareEncrypter[qRowIndex][0]
+                else:
+                    result += squareEncrypter[pRowIndex][pColumnIndex + 1]
+                    result += squareEncrypter[qRowIndex][qColumnIndex + 1]
+            elif (p == q):
+                result += p
+                result += 'X'
+                result += q
+            else:
+                result += squareEncrypter[qRowIndex][pColumnIndex]
+                result += squareEncrypter[pRowIndex][qColumnIndex]
+
+            lenghtResultText += 2
+
+        return result
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-encrypter',
+    version='0.1',
+    license='MIT',
+    description='An package include functions square encryptor',
+    install_requires=['numpy'],
+    url='https://github.com/PJasiczek/python-package-encrypter',
+    author='Piotr Jasiczek',
+    author_email='225933@student.pwr.edu.pl'
+)
